68b209859a745ac0028baeaca0a639eefb573be1
fan yang <yfzc1526@outlook.com>
2018-12-25 11:52:23 +0800

    Signed-off-by: fan yang <yfzc1526@outlook.com>

--- Config/push.bat
+++
@@ -1,8 +0,0 @@
-
-cd pure_code_practise && git ps
-cd ../pure_practice && git ps
-cd ../xmind && git ps
-cd ../mics && git ps
-
-
-pause
--- Config/pushAll.bat
+++
@@ -1,9 +0,0 @@
-
-cd pure_code_practise && git ps
-cd ../pure_practice && git ps
-cd ../xmind && git ps
-cd ../mics && git ps
-
-cd ../Reference && git ps
-cd ../kbengine && git ps
-pause
