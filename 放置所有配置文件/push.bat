11a734fd49c54a319bfe8524a450f4060391cb30
fan yang <yfzc1526@outlook.com>
2019-01-02 16:28:12 +0800

    1

--- 放置所有配置文件/push.bat
+++ 放置所有配置文件/push.bat
@@ -1,8 +1,15 @@
 
-cd pure_code_practise && git ps
-cd ../pure_practice && git ps
-cd ../xmind && git ps
-cd ../mics && git ps
 
+rem cd ../mics && git ps
 
+cd pure_code_practise && git cps
+cd ../pure_practice && git cps
+cd ../CsharpCode && git cps
+cd ../tmp && git cps
+cd ../common && git cps
+cd ../Mics && git cps
+
+cd ../OSM-Master && git cps
+cd ../Scut && git cps
+cd ../EntityComponentSystemSamples && git cps
 pause
---
+++ 放置所有配置文件/syhub.bat
@@ -0,0 +1,15 @@
+
+
+rem cd ../mics && git ps
+
+cd pure_code_practise && git mg
+cd ../pure_practice && git mg
+cd ../CsharpCode && git mg
+cd ../tmp && git mg
+cd ../common && git mg
+cd ../Mics && git mg
+
+cd ../OSM-Master && git mg
+cd ../Scut && git mg
+cd ../EntityComponentSystemSamples && git mg
+pause
