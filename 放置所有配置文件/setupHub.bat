ffc14635692e90004a990c6f4129732399ff6f38
fan yang <yfzc1526@outlook.com>
2019-01-04 00:02:17 +0800

    1

---
+++ 放置所有配置文件/setupHub.bat
@@ -0,0 +1,16 @@
+
+
+rem cd ../mics && git ps
+git clone  https://github.com/yangfan111/pure_code_practise.git
+git clone  https://github.com/yangfan111/pure_practice.git
+git clone  https://github.com/yangfan111/CsharpCode.git
+git clone  https://github.com/yangfan111/tmp.git
+git clone  https://github.com/yangfan111/common.git
+git clone  https://github.com/yangfan111/Mics.git
+git clone  https://github.com/yangfan111/OneNote.git
+git clone  https://github.com/yangfan111/OSM-Master.git
+git clone  https://github.com/yangfan111/Scut.git
+git clone  https://github.com/yangfan111/EntityComponentSystemSamples.git
+
+
+pause
